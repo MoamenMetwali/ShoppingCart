b5771a90c48a2c996153019f7ea37b69ccd06131
MoamenMetwali <muemen.batrawi@gmail.com>
2020-12-20 07:16:50 +0200

    Adding Category Model/Controller/Views

--- ShoppingCart/Startup.cs
+++ ShoppingCart/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingCart.Data;
 
 namespace ShoppingCart
 {
@@ -23,6 +25,9 @@ namespace ShoppingCart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(
+                Configuration.GetConnectionString("MainConnection")));
             services.AddControllersWithViews();
         }
 
